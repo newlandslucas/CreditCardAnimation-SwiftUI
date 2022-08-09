1d8d0204a2f17c7c14f26b75e4041c752cc7f414
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-08-09 14:33:26 -0300

    start project

---
+++ CreditCardAnimation/CreditCardAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardAnimationApp.swift
+//  CreditCardAnimation
+//
+//  Created by Lucas Newlands on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
