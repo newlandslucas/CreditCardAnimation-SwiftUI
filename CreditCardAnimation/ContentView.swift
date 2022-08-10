ecc4608d20768e66a461bfeab1e3c2a6c61b0a3f
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-08-09 22:36:10 -0300

    project finished

--- CreditCardAnimation/ContentView.swift
+++ CreditCardAnimation/ContentView.swift
@@ -12,26 +12,99 @@ struct ContentView: View {
     @State private var degress: Double = 0
     @State private var flipped: Bool = false
     
+    @State private var name: String = ""
+    @State private var cardNumber: String = ""
+    @State private var expires: String = ""
+    @State private var cvv: String = ""
+    
     var body: some View {
-        CreditCard{
-            VStack {
-                Group {
-                    if flipped {
-                        CreditCardBack()
-                    } else {
-                        CreditCardFront()
-                    }
+        VStack() {
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Pagamento")
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                        .foregroundColor(.black)
                 }
-                .rotation3DEffect(.degrees(degress),
-                                  axis: (x: 0.0, y: 1.0, z: 0.0))    
+                .padding()
+                
+                Spacer()
+                
             }
+            
+            
+            CreditCard{
+                VStack {
+                    Group {
+                        if flipped {
+                            CreditCardBack(cvv: cvv)
+                        } else {
+                            CreditCardFront(name: name, cardNumber: cardNumber, expires: expires)
+                        }
+                    }
+                    .rotation3DEffect(.degrees(degress),
+                                      axis: (x: 0.0, y: 1, z: 0.0))
+                }
 
+                }
+                .onTapGesture {
+                withAnimation {
+                    degress += 180
+                    flipped.toggle()
+                }
             }
-            .onTapGesture {
-            withAnimation {
-                degress += 180
-                flipped.toggle()
+            
+            VStack(spacing: 3) {
+                TextField("Nome", text: $name)
+                    .foregroundColor(.black)
+                    .font(.headline)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding([.top, .leading, .trailing])
+                
+                TextField("Número do cartão", text: $cardNumber).font(.headline)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding([.top, .leading, .trailing])
+                
+                TextField("Data de expiração", text: $expires).font(.headline)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding([.top, .leading, .trailing])
+            
+                
+                TextField("CVV", text: $cvv) { (editingChanged) in
+                    
+                    if editingChanged {
+                        withAnimation {
+                            degress += 180
+                            flipped.toggle()
+                        }
+                    } else {
+                        withAnimation {
+                            degress += 180
+                            flipped.toggle()
+                        }
+                    }
+                    
+                } onCommit: {}
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding([.top, .leading, .trailing])
             }
+ 
+            
+            Spacer()
+            
+            Button {
+                print("Comprou")
+            } label: {
+                HStack {
+                    Text("Finalizar")
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                }
+                .frame(width: 350, height: 50)
+                .foregroundColor(.white)
+                .background(.blue)
+                .cornerRadius(20)
+            }
+            
         }
     }
 }
--- CreditCardAnimation/CreditCard.swift
+++ CreditCardAnimation/CreditCard.swift
@@ -18,33 +18,143 @@ struct CreditCard<Content>: View where Content: View {
 }
 
 struct CreditCardFront: View {
+    
+    let name: String
+    let cardNumber: String
+    let expires: String
+    
     var body: some View {
         
-        VStack {
-            Text("Front")
-                .foregroundColor(.white)
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("GOL Smiles")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    
+                    Image(systemName: "dot.radiowaves.forward")
+                        .foregroundColor(.white)
+                        .font(.system(size: 24))
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .center, spacing: 1) {
+                    Text("VISA")
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .font(.system(size: 24))
+                    
+                    Text("Infinite")
+                        .foregroundColor(.gray)
+                        .fontWeight(.bold)
+                        .font(.system(size: 12))
+                }
+
+            }
+            
+            Spacer()
+            
+        
+            
+            Spacer()
+            
+            HStack {
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 10) {
+                        
+                        Text(name)
+                            .font(.system(size: 14))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        
+                        Text(cardNumber)
+                            .foregroundColor(.white)
+                            .font(.system(size: 14))
+                    }
+                    
+                    Spacer()
+                    
+                    HStack {
+
+                        Text("EXP DATE: ")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 12))
+                        
+                        Text(expires)
+                            .foregroundColor(.white)
+                            .font(.system(size: 16))
+                        
+                    }
+                    
+                }
+                
+            }
+            
+            
         }
-        .frame(width: 300, height: 200)
-        .background(LinearGradient(colors: [Color.purple, Color.blue], startPoint: .leading, endPoint: .trailing))
+        .frame(width: 330, height: 180)
+        .padding()
+        .background(LinearGradient(colors: [Color.black, Color.black], startPoint: .leading, endPoint: .trailing))
         .cornerRadius(10)
     }
 }
 
 struct CreditCardBack: View {
+    
+    let cvv: String
+    
     var body: some View {
         
-        VStack {
-            Text("")
-                .foregroundColor(.white)
+        VStack(spacing: 15) {
+            
+            
+            HStack {
+                
+            }
+            .frame(width: 360, height: 30)
+            .background(.gray)
+            .cornerRadius(10)
+            
+            Spacer()
+            
+            
+            HStack {
+                VStack(alignment: .trailing, spacing: 10) {
+                    
+                    HStack {
+
+                        Text(cvv)
+                            .foregroundColor(.white)
+                            .font(.system(size: 16))
+                            .rotation3DEffect(.degrees(180),
+                                            axis: (x: 0.0, y: 1.0, z: 0.0))
+                        
+                        Text("SECURITY CODE: ")
+                            .foregroundColor(.gray)
+                            .font(.system(size: 12))
+                            .rotation3DEffect(.degrees(180),
+                                            axis: (x: 0.0, y: 1.0, z: 0.0))
+                        
+                    }
+                    
+                    Spacer()
+                    
+                    
+                }
+                Spacer()
+            }
         }
-        .frame(width: 300, height: 200)
-        .background(LinearGradient(colors: [Color.black, Color.white], startPoint: .leading, endPoint: .trailing))
+        .frame(width: 330, height: 180)
+        .padding()
+        .background(LinearGradient(colors: [Color.black, Color.black], startPoint: .leading, endPoint: .trailing))
         .cornerRadius(10)
     }
 }
 
 struct CreditCard_Previews: PreviewProvider {
     static var previews: some View {
-        CreditCard<CreditCardFront>(content: {CreditCardFront() })
+        CreditCard<CreditCardFront>(content: {CreditCardFront(name: "Lucas S N MAchado", cardNumber: "0000 0000 0000 0000", expires: "00/00") })
     }
 }
